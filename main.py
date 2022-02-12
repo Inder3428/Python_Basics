e05080c79c6f1b2b178912d1c17119ab58257118
Inderpreet Singh <54847601+Inder3428@users.noreply.github.com>
2022-02-12 12:19:20 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# Inderptreet Singh
+
+# Simple Print Statements.
+print("Hello World")
+
+# Print statement using variables and Input statement:
+
+F_name = input("Enter Your First Name: ")
+L_name = input("Enter your Last Name: ")
+full_name = F_name + " " + L_name
+age: str = input("Please enter your age: ")
+print('\n Please review your Information', '\nYour Full name is:', full_name, '\n Your age is:', age)
