4e33424a307853b4f19928dbc74a7a6114996def
Inderpreet Singh <54847601+Inder3428@users.noreply.github.com>
2022-02-13 11:19:16 -0500

    Add files via upload

---
+++ Basic_Math_Oper.py
@@ -0,0 +1,21 @@
+# Inderpreet Singh.
+# program: Basic Math Operations:
+
+# addition, Subtraction, Multiplication, Division, Exponential, Module.
+a = 5
+b = 6
+# creating Variable add to perform addition between a and b
+add: float = a + b
+print('The Sum of a+b = ', add)
+
+Sub: float = a - b
+print('\nThe subtraction of a - b = ', Sub)
+
+mult: float = a * b
+print('\nThe Multiplication of a x b = ', mult)
+
+div: float = a / b
+print('\nThe division of a / b = ', div)
+
+exp: float = a ** b
+print('\nThe Sum of a^b = ', exp)
