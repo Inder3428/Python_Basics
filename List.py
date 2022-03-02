17446f42046f79f73eefb409fe924e61256eed58
Inderpreet Singh <54847601+Inder3428@users.noreply.github.com>
2022-03-01 20:12:49 -0500

    Create List.py

---
+++ List.py
@@ -0,0 +1,11 @@
+# creating List
+list = [1, 3, 5, 7, 9]
+print(list)
+print(type(list))
+
+list_cars = ['Honda', 'Toyota', 'BMW']
+print("\n", list_cars)
+print(type(list_cars))
+
+Drinks_ch = ['Sprite', 'Grape Soda', 'Lemonade']
+print('\n', Drinks_ch)
