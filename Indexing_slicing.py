030d2015d73922352b9c56a2732db3e7b42b3e01
Inderpreet Singh <54847601+Inder3428@users.noreply.github.com>
2022-03-02 15:56:24 -0500

    Update and rename main.py to Indexing_slicing.py

---
+++ Indexing_slicing.py
@@ -0,0 +1,22 @@
+# Inderpreet Singh
+
+# creating list
+Drinks_ch = ['Sprite', 'Grape Soda', 'Lemonade', 'DR Peper', 'Pepsi', 'Cola', 'Sparkling water']
+# item address   0     ,     1     ,      2    ,    3      ,     4  ,   5   ,          6
+
+print('\n', Drinks_ch)  # Print whole list
+
+# Indexing List
+print("\n", Drinks_ch[0])  # Print only First item in the list
+
+print("\n", Drinks_ch[4])  # Print Only last item in the list
+
+print("\n", Drinks_ch[6])  # Print Only last item in the list
+
+# Slicing the List
+print("\n", Drinks_ch[2:4])  # Print item 2 and 4 only [start: End]
+
+print("\n", Drinks_ch[1:5])  # Print from 1 to 5
+
+# slicing style to mention items from  yo 5
+print("\n", Drinks_ch[:5])  # Print whole list until item 5
